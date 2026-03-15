f1c5d04709956ab628271fadad4286acb521fb67
Infuriating <willsmithlikescat24@gmail.com>
2026-03-15 22:09:00 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom'
 import { useEffect } from 'react'
+import { useAuth } from './hooks/useAuth'
 import { AuthProvider } from './context/AuthContext'
 import { NotificationProvider } from './context/NotificationContext'
 

78b576f6bfd098238f0097a17397002d2b182ae2
Infuriating <willsmithlikescat24@gmail.com>
2026-03-16 12:04:50 +0000

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import { useSearchParams, useNavigate } from 'react-router-dom'
 import { useAuth } from '../hooks/useAuth'
 import { ChevronRight } from 'lucide-react'
